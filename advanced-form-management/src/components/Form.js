89f5b661b22d46a40e2aeb728494cb8543326406
derek-cornelison <dcornelison1216@gmail.com>
2020-04-28 03:06:16 -0400

    form styling

--- advanced-form-management/src/components/Form.js
+++ advanced-form-management/src/components/Form.js
@@ -1,23 +1,55 @@
 import React from 'react';
-import styled from './styled-components';
+import styled from 'styled-components';
+import * as yup from 'yup';
 
 const FormDiv = styled.div`
 display: flex;
 flex-direction: column;
 `;
 
+const SubmitButton = styled.button`
+font-size: 1.6rem;
+background-color: lightgray;
+border-radius: 5px;
+`;
+
+const Label = styled.label`
+font-size: 1.5rem;
+margin: .5rem;
+`;
+
+const Input = styled.input`
+background-color: lightskyblue;
+margin-left: .5rem;
+border-radius: 3px;
+`;
+
 const Form = () => {
   return (
     <FormDiv>
-      <label htmlFor="name">
+      <Label htmlFor="name">
         Name:
-        <input id="name" type="text" name="name" />
-      </label>
+        <Input id="name" type="text" name="name" />
+      </Label>
 
-      <label htmlFor="email">
+      <Label htmlFor="email">
         Email:
-        <input id="email" type="text" name="email" />
-      </label>
+        <Input id="email" type="text" name="email" />
+      </Label>
+
+      <Label htmlFor="password">
+        Password:
+        <Input id="password" type="text" name="password" />
+      </Label>
+
+      <Label htmlFor="terms">
+        Terms & Conditions
+        <Input type="checkbox" name="terms" id="terms" checked={false} />
+      </Label>
+
+      <Label htmlFor="submit">
+        <SubmitButton type="submit" disabled={false}>Submit</SubmitButton>
+      </Label>
     </FormDiv>
   );
 }
