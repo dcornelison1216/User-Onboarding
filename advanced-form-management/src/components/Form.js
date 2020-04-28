21458a35bbdea4c9fcd90eb77475c43b41f94037
derek-cornelison <dcornelison1216@gmail.com>
2020-04-28 01:51:23 -0400

    added Form.js and imported to App.js

---
+++ advanced-form-management/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <label htmlFor="name">
+      <input id="name" type="text" name="name" />
+    </label>
+  );
+}
+
+export default Form;
