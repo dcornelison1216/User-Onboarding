22b4ac1318c7541ca3655ba4a2ab6f0ec053f7f6
derek-cornelison <dcornelison1216@gmail.com>
2020-04-28 01:58:43 -0400

    adding input elements to Form.js

--- advanced-form-management/src/components/Form.js
+++ advanced-form-management/src/components/Form.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from './styled-components';
+
+const FormDiv = styled.div`
+display: flex;
+flex-direction: column;
+`;
 
 const Form = () => {
   return (
-    <label htmlFor="name">
-      <input id="name" type="text" name="name" />
-    </label>
+    <FormDiv>
+      <label htmlFor="name">
+        Name:
+        <input id="name" type="text" name="name" />
+      </label>
+
+      <label htmlFor="email">
+        Email:
+        <input id="email" type="text" name="email" />
+      </label>
+    </FormDiv>
   );
 }
 
